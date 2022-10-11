04dd72823701b7d73d5e2ce9f62ebf13824ea094
bharathmb-crack <bharathmbyahtti@gmail.com>
2022-10-11 23:58:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskname: newTask,
+    };
+    setTodoList([...todoList, task]);
+  };
+
+  const deletetask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todotask">
+        <div className="inputTask">
+          <input onChange={handleChange} type="text" />
+          <button onClick={addTask}>Add Task</button>
+        </div>
+        {todoList.map((task) => {
+          return (
+            <div className="showtask">
+              <h2>{task.taskname}</h2>
+              <button onClick={() => deletetask(task.id)}>X</button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
